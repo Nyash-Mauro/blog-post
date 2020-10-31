6a702104b0a97ecc486e98c8323403c4e5d840a5
Nyash-Mauro <mauricee423@gmail.com>
2020-11-01 01:46:07 +0300

    added function to idisplay home and post

--- app/main/views.py
+++ app/main/views.py
@@ -4,3 +4,18 @@ from flask_login import login_required,current_user
 from app.models import User,Post,Comment
 from .. import db,photos
 from app.requests import get_Quotes
+
+
+@main.route('/')
+def home():
+    quotes=get_Quotes()
+    print(quotes)
+   
+    return render_template('index.html',quotes=quotes)
+
+@main.route('/display_all', methods= ['POST','GET'])
+def displayposts():
+     posts = Post.query.all()
+     quote = get_Quotes()
+     print(quote)
+     return render_template('display_posts.html',posts=posts,quote=quote)
