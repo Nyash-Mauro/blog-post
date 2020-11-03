2d2b6a76a25a671d1622c63a6d2ada4e14762bf7
Nyash-Mauro <mauricee423@gmail.com>
2020-11-03 13:02:58 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='nyaga'
 export APP_SETTINGS="config.DevelopmentConfig"
-export DATABASE_URL='postgresql+psycopg2://moringa:72330000@localhost/BLOG'
+export DATABASE_URL='postgresql+psycopg2://moringa:JUSTCLAUSEKOTLIN6@localhost/BLOG'
 
 python3.6 manage.py 
